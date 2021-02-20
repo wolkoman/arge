2c3b0fa5d8b572c448d74145cea70c81b9807186
Manuel <manuel.wolkowitsch@gmail.com>
2021-02-20 23:38:18 +0100

    Change frontpage image

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,12 @@ export default function Home({articles, cockpitHost}) {
   return (
     <Site responsive={false}>
       <Title/>
+      <img src="assets/Artboard 2@2x.png" className="absolute right-0 mt-80"/>
       <Responsive>
         {articles.map((entry, i) => (
           <div className={`py-8 flex flex-col md:flex-row ${i % 2 === 0 ? '' : 'md:flex-row-reverse'}`} key={entry._id}>
             <div className={`md:w-1/3 ${i % 2 === 0 ? 'mr-4' : 'ml-4'} md:h-64 h-64`}>
-              <div className="w-full h-full border-2 border-primary-500 rounded" style={{
+              <div className="w-full h-full border border-primary-500 rounded" style={{
                 backgroundImage: `url(${cockpitHost}/${entry.image.path})`,
                 backgroundSize: 'cover',
                 backgroundPosition: '50% 50%'
@@ -48,7 +49,7 @@ export async function getStaticProps() {
 const Title = () => (
   <div className="flex flex-col md:flex-row w-full h-6xl md:h-3xl">
     <div className="max-w-7xl flex-0 md:w-2/3 w-full relative -top-24">
-      <img className="w-full" src="assets/cbanner.png"/>
+      <img className="w-full" src="assets/Artboard 1@2x.png"/>
     </div>
     <div className="flex flex-col p-6 leading-10 justify-center h-full -top-24 md:top-0 md:-left-24 relative font-bold">
       <div className="text-6xl text-primary-500">
--- pages/spenden.tsx
+++ pages/spenden.tsx
@@ -1,6 +1,16 @@
-import React from "react";
-import { Site } from "../components/Site";
+import React from 'react';
+import {Site} from '../components/Site';
+import ReactMarkdown from 'react-markdown';
+import {renderer} from '../util/markdownRenderer';
+import {fetchSingleton} from '../util/cockpit';
 
-export default function Home() {
-  return <Site>Spenden</Site>;
+export default function Spenden({article}) {
+  return <Site>
+    <div className="text-5xl font-bold mb-6">Spenden</div>
+    <ReactMarkdown renderers={renderer} children={article.content}/>
+  </Site>;
+}
+
+export async function getStaticProps() {
+  return {props: {article: await fetchSingleton('spenden')}}
 }
