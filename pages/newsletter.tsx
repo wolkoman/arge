90ee3cd340fd928524f36de48deb23eaae9d7470
Manuel <manuel.wolkowitsch@gmail.com>
2021-08-08 22:04:03 +0200

    Fix compilation error

--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -7,7 +7,7 @@ export default function Newsletter({article}) {
   return <Site>
     <div className="text-5xl font-bold mb-6 text-primary-500">Newsletter</div>
     <Markdown children={article.content}/>
-    <iframe className="mj-w-res-iframe" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://app.mailjet.com/widget/iframe/6CzS/K8x" width="100%" height="400px"></iframe>
+    <iframe className="mj-w-res-iframe" frameBorder="0" scrolling="no" src="https://app.mailjet.com/widget/iframe/6CzS/K8x" width="100%" height="400px"/>
     <script type="text/javascript" src="https://app.mailjet.com/statics/js/iframeResizer.min.js"></script>
   </Site>;
 }
