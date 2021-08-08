9ba266d31e0b87bbe2f47f90887ccfa6770517b2
Manuel <manuel.wolkowitsch@gmail.com>
2021-08-08 21:09:36 +0200

    Add photoswipe gallery

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,8 +1,61 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown';
+import {Gallery, Item} from 'react-photoswipe-gallery';
+import 'photoswipe/dist/photoswipe.css'
+import 'photoswipe/dist/default-skin/default-skin.css'
 
 export default function Markdown({children}) {
-  return <ReactMarkdown renderers={renderer} children={children} allowDangerousHtml={true}/>
+  return <>{ReactMarkdown({renderers: renderer, children, allowDangerousHtml: true}).props.children.map(mapObject)}</>;
+}
+
+const mapObject = (object) => {
+  return {
+    ...object,
+    props: mapProps(object.props)
+  };
+}
+const mapProps = (props) => {
+  return {
+    ...props,
+    children: mapChildren(props.children)
+  };
+}
+const mapChildren = (children) => {
+  if(Array.isArray(children) && children.length > 1 && children.every(isPartOfGallery)) {
+    const imageSources = children.filter(isImageElement).map(imageElement => imageElement.props.src);
+    return <Gallery>
+      {imageSources.map(source =><Item
+        key={source}
+        original={source}
+        thumbnail={source}
+        width="1000"
+        height="1000"
+      >
+        {({ ref, open }) => (
+          <>
+          { /* @ts-ignore */ }
+          <img ref={ref} onClick={open} src={source} className="rounded border-primary-500 border m-2 inline-block h-32 cursor-pointer"  />
+          </>
+        )}
+      </Item>)}
+    </Gallery>;
+  }else if(Array.isArray(children)) {
+    return children.map(mapObject);
+  } else {
+    return children;
+  }
+}
+
+function isImageElement(object) {
+  return !!object.props.src;
+}
+
+function isLineBreakElement(object) {
+  return object.props.value === '\n';
+}
+
+const isPartOfGallery = (object) => {
+  return isImageElement(object) || isLineBreakElement(object);
 }
 
 const renderer = {
@@ -15,11 +68,15 @@ const renderer = {
   link: value => (
     <a href={value.href} className="text-primary-500 underline hover:no-underline">{value.children}</a>),
   image(value) {
+    let size = 28;
+    if(value.src.match("size=(\d*)")) {
+      size = value.src.split("size=")[1];
+    }
     if(value.src.match("[^\|]*#([^\|]*)")){
       const parts = value.src.split('#');
-      return <a href={parts[1]}><img className="rounded border-primary-500 border m-2 max-w-md w-full" src={parts[0]}/></a>;
+      return <a href={parts[1]}><img className="rounded border-primary-500 border m-2 w-full" src={parts[0]} style={{ maxWidth: `${size}rem` }}/></a>;
     }
-    return <img className="rounded border-primary-500 border m-2 max-w-md w-full" src={value.src}/>;
+    return <img className="rounded border-primary-500 border m-2 w-full" src={value.src} style={{ maxWidth: `${size}rem` }}/>;
   },
   paragraph: value => (
     <div className="py-2" children={value.children} />),
