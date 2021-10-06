d3909b4f65c9b494e16e5b6a132f32dd55426e57
Manuel <manuel.wolkowitsch@gmail.com>
2021-10-06 09:15:11 +0200

    Set payment attribute

--- pages/spenden.tsx
+++ pages/spenden.tsx
@@ -8,7 +8,7 @@ export default function Spenden({article}) {
     <div className="text-5xl font-bold mb-6 text-primary-500">Spenden</div>
     <div className="flex flex-col md:flex-row">
       {/* @ts-ignore */ }
-      <iframe allowpaymentrequest className="flex-shrink-0 pr-4" src="https://donorbox.org/embed/arge-schopfungsverantwortung?default_interval=o&hide_donation_meter=true"
+      <iframe allowpaymentrequest="true" className="flex-shrink-0 pr-4" src="https://donorbox.org/embed/arge-schopfungsverantwortung?default_interval=o&hide_donation_meter=true"
     name="donorbox" frameBorder="0" scrolling="no"
     height="800px" width="100%" style={{maxWidth: 400, minWidth: 250, maxHeight: 'none!important'}}/>
       <div><Markdown children={article.content}/></div>
