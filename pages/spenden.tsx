17d01d56a540b5536e8967f5a1f026fc36912420
Manuel Wolkowitsch <m.wolkowitsch@firestart.com>
2021-12-11 10:42:46 +0100

    Update spenden

--- pages/spenden.tsx
+++ pages/spenden.tsx
@@ -6,13 +6,11 @@ import Markdown from '../components/Markdown';
 export default function Spenden({article}) {
   return <Site>
     <div className="text-5xl font-bold mb-6 text-primary-500">Spenden</div>
-    <div className="flex flex-col-reverse md:flex-row  space-x-4">
-      <div className="flex flex-col items-center w-full bg-primary-500 text-white px-2 py-12 rounded text-center text-xl">
-        Wir freuen uns über Ihre Spenden an das Konto:
-        <div className="font-black mt-4">AT12 2011 1292 5730 9601</div>
-        <div className="mb-4 text-sm italic">Arbeitsgemeinschaft Schöpfungsverantwortung</div>
-        <div className="text-xs mt-14">Wir arbeiten derzeit noch an einer einfachen Möglichkeit schnell und unkompliziert online zu spenden. </div>
-      </div>
+    <div className="flex flex-col md:flex-row">
+      {/* @ts-ignore */ }
+      <iframe allowpaymentrequest="true" className="flex-shrink-0 pr-4" src="https://donorbox.org/embed/arge-schopfungsverantwortung?default_interval=o&hide_donation_meter=true"
+    name="donorbox" frameBorder="0" scrolling="no"
+    height="800px" width="100%" style={{maxWidth: 400, minWidth: 250, maxHeight: 'none!important'}}/>
       <div><Markdown children={article.content}/></div>
     </div>
 
