db70eee51290f4aa3b19c0b6efba816d6753cdf9
Manuel <manuel.wolkowitsch@gmail.com>
2022-03-11 17:18:40 +0100

    Remove page update

--- components/HierachyArticles.tsx
+++ components/HierachyArticles.tsx
@@ -3,51 +3,51 @@ import {cockpitHost, fetchCollection} from '../util/cockpit';
 import {encodeSlug} from '../util/slug';
 import {ExtendedLayout} from './ExtendedLayout';
 
-const system = (collectionName: string, url:string, displayName: string) => ({
-  component: ({cockpitHost, item, children, parents}) =>  <ExtendedLayout
-          crums={[{name: displayName, link: `/${url}`},
+const system = (collectionName: string, url: string, displayName: string) => ({
+    component: ({cockpitHost, item, children, parents}) => <ExtendedLayout
+        crums={[{name: displayName, link: `/${url}`},
             ...parents.map(({name, segments}) => ({
-              name,
-              link: `/${url}/${segments.join('/')}`
+                name,
+                link: `/${url}/${segments.join('/')}`
             })), {name: item.title}]}
-          title={item.title}
-          files={item.files}
-          subsites={children}
-          content={item.content}
-          cockpitHost={cockpitHost}
-  />,
+        title={item.title}
+        files={item.files}
+        subsites={children}
+        content={item.content}
+        cockpitHost={cockpitHost}
+    />,
     paths: async () => {
-      const items = (await fetchCollection(collectionName));
-      const hierachies = items.map(item => getCategoryUrl(item, items));
-      return {paths: hierachies.map(hierachy => ({params: {hierachy}})), fallback: false};
+        const items = (await fetchCollection(collectionName));
+        const hierachies = items.map(item => getCategoryUrl(item, items));
+        return {paths: hierachies.map(hierachy => ({params: {hierachy}})), fallback: false};
     },
     props: async ({params}) => {
-      const items = await fetchCollection(collectionName);
-      const slug = params.hierachy.reverse()[0];
-      const item = items.find(item => encodeSlug(item.title) === slug);
-      const children = items
-      .filter(t => t.category?._id === item._id && t.hidden !== true)
-          .map(t => ({
-            slug: encodeSlug(t.title),
-            title: t.title,
-            priority: t.priority ?? 100
-          }));
-      const parents = [];
-      let movingItem = item;
-      while (movingItem?.category?._id) {
-        movingItem = items.find(t => t._id === movingItem?.category._id);
-        if(movingItem === undefined) break;
-        parents.unshift({name: movingItem.title, segments: getCategoryUrl(movingItem, items)})
-      }
-      return {props: {cockpitHost, item, children, parents}, revalidate: 1}
+        const items = await fetchCollection(collectionName);
+        const slug = params.hierachy.reverse()[0];
+        const item = items.find(item => encodeSlug(item.title) === slug);
+        const children = items
+            .filter(t => t.category?._id === item._id && t.hidden !== true)
+            .map(t => ({
+                slug: encodeSlug(t.title),
+                title: t.title,
+                priority: t.priority ?? 100
+            }));
+        const parents = [];
+        let movingItem = item;
+        while (movingItem?.category?._id) {
+            movingItem = items.find(t => t._id === movingItem?.category._id);
+            if (movingItem === undefined) break;
+            parents.unshift({name: movingItem.title, segments: getCategoryUrl(movingItem, items)})
+        }
+        return {props: {cockpitHost, item, children, parents}, revalidate: 1}
     }
-  });
+});
 
 export const getCategoryUrl = (item, items) => {
-  const parentItem = items.find(t => t._id === item?.category?._id);
-  if(parentItem === undefined){
-    return [encodeSlug(item.title)];
-  }
-  return [...(item?.category ? getCategoryUrl(parentItem, items) : []), encodeSlug(item.title)];
+    const parentItem = items.find(t => t._id === item?.category?._id);
+    if (parentItem === undefined) {
+        return [encodeSlug(item.title)];
+    }
+    return [...(item?.category ? getCategoryUrl(parentItem, items) : []), encodeSlug(item.title)];
 };
 export default system;
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link';
-import React, {useState} from 'react';
+import React from 'react';
 import {Site} from '../../components/Site';
 
-export default function Spenden({}) {
-  const [showUpdate, setShowUpdate] = useState(false);
+export default function Spenden() {
 
   return <Site title="Admin">
     <div className="mt-8 mb-1 text-primary-500 text-lg font-bold">Seitenstruktur</div>
@@ -13,7 +12,7 @@ export default function Spenden({}) {
       </div>
     </Link>
     <div className="mt-8 mb-1 text-primary-500 text-lg font-bold">Standard Design</div>
-    <Link href="admin/design">dd
+    <Link href="admin/design">
       <div className="my-1 px-2 bg-blue-900 text-white rounded hover:opacity-90 inline-block cursor-pointer" style={{background: '#0e1e25'}}>
         ansehen
       </div>
