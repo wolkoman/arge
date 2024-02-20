286b32899198b8ec375f1d3d840e0c9525e12658
Manuel Wolkowitsch <manuel@magic.dev>
2024-02-20 12:57:40 +0100

    update

--- pages/gv.tsx
+++ pages/gv.tsx
@@ -12,11 +12,26 @@ export default function Home() {
     return (
         <Site responsive={false} title="Generalversammlung 2024">
             <div className="relative z-20 min-h-md">
-                <Responsive>
-                    <div className="text-3xl my-4 font-bold">Generalversammlung 2024</div>
-                    <div>Der Link zum Beitritt wird rechtzeitig hier veröffentlicht.</div>
-                  <div className="inline-block cursor-not-allowed px-4 py-2 opacity-50 bg-primary-500 text-white my-4 rounded-lg">Beitritt zum Online Meeting</div>
-                </Responsive>
+              <Responsive>
+                <div className="text-3xl my-4 font-bold">Generalversammlung 2024</div>
+                <div className="flex flex-col gap-2 my-4">
+                  <div>Die Generalversammlung findet am 24.02.2024 um 10:00 Uhr statt.</div>
+                  <div>Die Tagesordnung lautet:<br/>
+                    1. Feststellen der Beschlussfähigkeit<br/>
+                    2. Bericht des Vorstandes<br/>
+                    3. Finanzbericht und Entlastung der Vorstandes<br/>
+                    4. Wahl des Vorstandes<br/>
+                    5. Anpassung der Statuten<br/>
+                    6. Allfälliges
+                  </div>
+                  <div>Der Link zum Beitritt wird am Tag der Sitzung hier veröffentlicht:</div>
+
+                </div>
+                <div
+                  className="inline-block cursor-not-allowed px-4 py-2 opacity-50 bg-primary-500 text-white my-4 rounded-lg">Beitritt
+                  zum Online Meeting
+                </div>
+              </Responsive>
             </div>
         </Site>
     );
