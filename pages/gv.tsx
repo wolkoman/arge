76fbb9c068d931d38110f9b3f894c5aed4f902c0
Manuel Wolkowitsch <manuel@magic.dev>
2024-02-24 09:10:01 +0100

    update gv link

--- pages/gv.tsx
+++ pages/gv.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
                 </div>
                 <Link href="https://meet.jit.si/argegeneralversammlung24">
-                <div className="inline-block px-4 py-2 opacity-50 bg-primary-500 hover:bg-primary-400 text-white my-4 rounded-lg">
+                <div className="inline-block px-4 py-2 bg-primary-500 hover:bg-primary-400 text-white my-4 rounded-lg">
                   Beitritt zum Online Meeting
                 </div>
                 </Link>
